c45414739217b8efc3c60b2fdb4eb786b0e5006d
Jishnu-Diveroid <148034063+Jishnu-Diveroid@users.noreply.github.com>
2024-03-07 16:45:22 +0530

    add fragments and search bar

--- app/src/main/java/com/example/spotify/MainActivity.kt
+++ app/src/main/java/com/example/spotify/MainActivity.kt
@@ -1,21 +1,33 @@
 package com.example.spotify
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.spotify.ui.search.SearchFragment
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     val viewModel: SpotifyViewModel by viewModels()
+
     @Inject
     lateinit var repository: Repository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel.search()
-        Log.d("hilt", repository.hello().toString())
+
+        navigateToSearchFragment()
+
+    }
+
+    private fun navigateToSearchFragment() {
+        val fragment = supportFragmentManager.findFragmentByTag(SearchFragment.TAG)
+
+        supportFragmentManager.beginTransaction().replace(
+            R.id.fragment_container_view, fragment ?: SearchFragment.newInstance()
+        ).commit()
     }
 }
